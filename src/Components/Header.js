bab350ababebaf2f85578718ba8a9068bc761d0c
crystalbit <crystalbit@mail.ru>
2020-01-28 01:56:31 +0300

    return back login button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-//import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import logo from '../images/ninja.gif';
 import Strings from '../Helpers/Strings';
@@ -39,7 +39,7 @@ export default function(props) {
                     <Typography variant="h6">
                         {Strings.shop.name}
                     </Typography>
-                    {/* <Button color="inherit">Login</Button> */}
+                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </div>
