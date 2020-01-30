524c26293e804027b29b9faf0e1ac827644f3a0a
crystalbit <crystalbit@mail.ru>
2020-01-31 01:06:01 +0300

    replace email regex

--- src/Helpers/validateClient.js
+++ src/Helpers/validateClient.js
@@ -1,6 +1,6 @@
 import phoneMask from './phoneMask';
 
-const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const emailRegexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 /**
  * @param {object} client - client data
